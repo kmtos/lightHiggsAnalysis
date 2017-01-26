2a07a9ce501cf3f38059fa686d1fc687764b1b31
Mengyao Shi <mengyao.shi@cern.ch>
2017-01-26 17:49:46 +0100

    organize folders QCDBackground and StandardPlotFormat

---
+++ GGHAA2Mu2TauAnalysis/QCDBackground/lxplusbatchscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SCRAM_ARCH=slc6_amd64_gcc530
+cd /afs/cern.ch/work/m/mshi/private/CMSSW_8_0_17/src
+eval `scramv1 runtime -sh`
+cd -
+source /afs/cern.ch/cms/caf/setup.sh
+CFG_FILE="Copy.py"
+cmsRun /afs/cern.ch/work/m/mshi/private/CMSSW_8_0_17/src/GGHAA2Mu2TauAnalysis/QCDBackground/$CFG_FILE > Copy.txt
+rfcp Copy.txt /afs/cern.ch/work/m/mshi/private/CMSSW_8_0_17/src/GGHAA2Mu2TauAnalysis/QCDBackground/Copy.txt
+rfcp Copy.root /afs/cern.ch/work/m/mshi/private/CMSSW_8_0_17/src/GGHAA2Mu2TauAnalysis/QCDBackground/Copy.root
+
+exit 0
