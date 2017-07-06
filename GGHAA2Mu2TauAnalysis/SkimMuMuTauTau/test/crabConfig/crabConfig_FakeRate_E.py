3b9ce310cc5e08c341d3b84d0b9fef50f6b14a30
Kyle <kmtos@ucdavis.edu>
2017-07-06 22:50:07 +0200

    Updating after adding new cfg and crab files for each era.

---
+++ GGHAA2Mu2TauAnalysis/SkimMuMuTauTau/test/crabConfig/crabConfig_FakeRate_E.py
@@ -0,0 +1,22 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'DY_FakeRate_Inv81to101_Mu50_MedMuID_JUL3_E'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../SkimSequence/TestRegionsSkim_Crab_FakeRate_E.py' #_HLT2.py'
+
+config.Data.inputDataset = '/SingleMuon/Run2016E-23Sep2016-v1/AOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob =  275
+config.Data.totalUnits = 3000000
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+config.Data.outLFNDirBase = '/store/group/phys_higgs/HiggsExo/ktos/' #'/store/user/ktos/' #'/store/group/phys_higgs/HiggsExo/ktos/' #'/store/user/ktos/'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'DY_FakeRate_Inv81to101_Mu50_MedMuID_JUL3_E'
+
+config.Site.storageSite = 'T2_CH_CERN'
